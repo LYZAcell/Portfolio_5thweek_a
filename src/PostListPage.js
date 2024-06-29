c1ff930a37a1748f91472b57d986135d5ac7a581
LYZAcell <applelgy@naver.com>
2024-06-29 22:34:10 +0900

    Revert "글목록 api 연결완료"
    This reverts commit 970b927b0fb4fb0040dfe87d5fb37be188d2eb35.

--- src/PostListPage.js
+++ src/PostListPage.js
@@ -1,19 +1,47 @@
-// PostListPage.js
+// PostListTable.js
+// PostListTable.js
 import React from 'react';
-import PostListTable from './PostListTable';
+import { usePostContext } from './PostContext';
+import "./Post_table.css";
+import { useNavigate } from 'react-router-dom';
 
-const PostListPage = () => {
+const PostListTable = () => {
+  const { posts } = usePostContext();
+  const navigate = useNavigate();
+
+  const truncateContent = (content, maxLength) => {
+    return content.length > maxLength ? content.slice(0, maxLength) + '...' : content;
+  };
+
   return (
-    <div>
-      <br/>
-      <h1 style={{ color: "white", textAlign: "center", fontSize : "30px"}}>Post List</h1>
-      <br/>
-      <PostListTable />
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Content</th>
+            <th>Author</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map((post, index) => (
+            <tr key={index} onClick={() => navigate(`/post/${post.id}`)}>
+              <td>{post.title}</td>
+              <td>{truncateContent(post.body, 50)}</td>
+              <td>{post.user}</td>
+              <td>{/* 필요한 경우 날짜 필드 추가 */}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
-export default PostListPage;
+export default PostListTable;
+
+
 
 
 {/*const PostListPage = () => {
--- src/server/index.js
+++ src/server/index.js
@@ -4,4 +4,4 @@ export const apiCall = axios.create({
     baseURL: "https://hufs-mutsa-12th.store/",
     // 회원가입시 /dj/registration 확인
     // 로그인 토큰 검사자체는 되는거같음.
-})
+});
